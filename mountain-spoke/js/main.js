29d073adf8037d216eaa880be9aed2c3d430c1f8
Ethan Russon <enrusson@gmail.com>
2018-04-11 18:24:35 -0600

    final
    test for json

--- mountain-spoke/js/main.js
+++ mountain-spoke/js/main.js
@@ -23,3 +23,27 @@ function showDivs(n) {
     }
     y[slideIndex-1].style.display = "block"; 
 }
+
+function expand1() {
+    $("#bike1").toggleClass("invisible");
+    $("#bike1-div").toggleClass("move-down");
+    $("#bike1-div").toggleClass("expand");
+}
+
+function expand2() {
+    $("#bike2").toggleClass("invisible");
+    $("#bike2-div").toggleClass("move-down");
+    $("#bike2-div").toggleClass("expand");
+}
+
+function expand3() {
+    $("#bike3").toggleClass("invisible");
+    $("#bike3-div").toggleClass("move-down");
+    $("#bike3-div").toggleClass("expand");
+}
+
+function thankYou() {
+    $("#thankyou").removeClass("hidden");
+    $("#submission").addClass("hidden");
+}
+    
